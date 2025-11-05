65a3c2f03314569ee7a723b99666bb008d40664c
mdeekshith00 <mdeekshith000@gmail.com>
2025-11-05 23:40:39 +0530

    added new userEvent class

---
+++ src/main/java/com/common/vo/UserEvent.java
@@ -0,0 +1,14 @@
+package com.common.vo;
+
+import lombok.Data;
+
+@Data
+public class UserEvent {
+
+	    private String eventType;
+	    private String userId;
+	    private String email;
+	    private String message;
+
+
+}
