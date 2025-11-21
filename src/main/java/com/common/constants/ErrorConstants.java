4f43df6d434aca0e9e5d2778b37cbec36421e8ed
mdeekshith00 <mdeekshith000@gmail.com>
2025-11-21 11:22:19 +0530

    removed unneccesary constants

--- src/main/java/com/common/constants/ErrorConstants.java
+++ src/main/java/com/common/constants/ErrorConstants.java
@@ -10,8 +10,6 @@ public class ErrorConstants {
 	public static final String INVALID_DATA = "INVALID.DATA";
 	public static final String API_FAILURE = "API.FAILURE";
 	public static final String PARSING_ERROR = "PARSING.ERROR";
-	public static final String BOOKING_UNAVAILABLE = "BOOKING.UNAVAILABLE";
-	public static final String CANCELLED_BOOKING = "CANCELLED.BOOKING";
 	public static final String DB_FAILURE = "DB.FAILURE";
 	public static final String PHONE_NUMBER_ALREADY_EXISTS = "PHONENUMBER.ALREADY.EXISTS";
 	public static final String SESSION_VALIDATION = "Session validation failed for : ";
@@ -23,22 +21,6 @@ public class ErrorConstants {
 	
 	
 	// Invalid data
-		public static final String INVALID_USER_ID_ERROR_MESSAGE = "Invalid user id passed.";
-		public static final String INVALID_CENTER_ID_ERROR_MESSAGE = "Invalid center id passed.";
-		public static final String INVALID_TECHNICIAN_ID_ERROR_MESSAGE = "Invalid technician id passed.";
-		public static final String INVALID_SERVICE_TYPE_ID_ERROR_MESSAGE = "Invalid servicetype id passed.";
-		public static final String INVALID_PREFERENCE_ID_ERROR_MESSAGE = "Invalid preference id";
-		public static final String INALID_IMAGE_ID_ERROR_MESSAGE = "Invalid image id passed.";
-		public static final String INALID_HIGHLIGHTS_ID_ERROR_MESSAGE = "Invalid center highlights id passed.";
-		public static final String INVALID_SERVICE_CENTER_ADMIN_ID_ERROR_MESSAGE = "Invalid service center admin id passed.";
-		public static final String INVALID_REFERENCE_KEY_ERROR_MESSAGE = "No record found with provided reference key.";
-		public static final String INVALID_BOOKING_ID_ERROR_MESSAGE = "Invalid booking id passed.";
-		public static final String INVALID_REVIEW_ID_ERROR_MESSAGE = "Invalid review id passed.";
-		public static final String INVALID_REPLY_ID_ERROR_MESSAGE = "Invalid reply id passed.";
-		public static final String INVALID_SERVICE_ID_ERROR_MESSAGE = "Invalid service id passed.";
-		public static final String SERVICE_CENTER_SLOT_NOT_FOUND_FOR_GIVEN_DATE_AND_SLOT = "Service center slot not found for given date and slot";
-		public static final String SERVICE_CENTER_SLOTS_ARE_NOT_EMPTY = "Service Center slots are not empty";
-		public static final String INVALID_LOCATION_ID_ERROR_MESSAGE = "Invalid service center location Id passed";
 		public static final String INVALID_DETAILS_FOR_REVIEW = "Provided details for submitting a review are not valid";
 		
 		
